091a21ebdea7af4524cccceeaeac60924bf06055
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-10-17 13:14:27 +0200

    completed

--- part01-Part01_05.Message/src/main/java/Message.java
+++ part01-Part01_05.Message/src/main/java/Message.java
@@ -7,7 +7,8 @@ public class Message {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Write a message:");
-        // Write your program here
+        String user = scanner.nextLine();
+        System.out.println(user);
 
     }
 }
