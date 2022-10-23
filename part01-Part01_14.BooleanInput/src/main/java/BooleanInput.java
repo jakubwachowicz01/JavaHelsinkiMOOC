16ac5eb8cd1dabc8f58adc1208e808c395d684bf
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-10-23 17:43:52 +0200

    completed

--- part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
+++ part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
@@ -6,7 +6,8 @@ public class BooleanInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // write your program here
-
+        System.out.println("Write something:");
+        Boolean input = Boolean.valueOf(scanner.nextLine());
+        System.out.println("True or false? " + input);
     }
 }
