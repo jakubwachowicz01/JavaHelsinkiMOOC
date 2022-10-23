0692d409e831972b996f4f1ed585c96c79049033
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-10-23 17:18:48 +0200

    completed

--- part01-Part01_10.Story/src/main/java/Story.java
+++ part01-Part01_10.Story/src/main/java/Story.java
@@ -6,7 +6,14 @@ public class Story {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
-
+        System.out.println("I will tell you a story, but I need some information first.");
+        System.out.println("What is the main character called?");
+        String firstInput = scanner.nextLine();
+        System.out.println("What is their job?");
+        String secondInput = scanner.nextLine();
+        System.out.println("Here is the story:\n" +
+                "Once upon a time there was " + firstInput + ", who was " + secondInput + ".\n" +
+                "On the way to work, " + firstInput + " reflected on life.\n" +
+                "Perhaps " + firstInput + " will not be " + secondInput + " forever.");
     }
 }
