3d29d4d96518cd1cf944650fa17c91b06d886399
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-10-23 17:11:55 +0200

    completed

--- part01-Part01_09.Conversation/src/main/java/Conversation.java
+++ part01-Part01_09.Conversation/src/main/java/Conversation.java
@@ -6,7 +6,10 @@ public class Conversation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
-
+        System.out.println("Greetings! How are you doing?");
+        String firstInput = scanner.nextLine();
+        System.out.println("Oh, how interesting. Tell me more!");
+        String secondInput = scanner.nextLine();
+        System.out.println("Thanks for sharing!");
     }
 }
