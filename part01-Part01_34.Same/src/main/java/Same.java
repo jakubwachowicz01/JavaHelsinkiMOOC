95b3d55f49047a2f3af536c15fdcd593d86b7273
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-11-17 12:33:41 +0100

    completed

--- part01-Part01_34.Same/src/main/java/Same.java
+++ part01-Part01_34.Same/src/main/java/Same.java
@@ -6,6 +6,15 @@ public class Same {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here. 
+        System.out.println("Enter the first string:");
+        String first = String.valueOf(scan.nextLine());
+        System.out.println("Enter the second string:");
+        String second = String.valueOf(scan.nextLine());
+
+        if(first.equals(second)){
+            System.out.println("Same");
+        }else{
+            System.out.println("Different");
+        }
     }
 }
--- part01-Part01_37.GiftTax/src/main/java/GiftTax.java
+++ part01-Part01_37.GiftTax/src/main/java/GiftTax.java
@@ -5,6 +5,22 @@ public class GiftTax {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Value of the gift?");
+        int value = Integer.valueOf(scan.nextLine());
+
+        if(value<5000){
+            System.out.println("No tax");
+        }else if(value<=25000){
+            System.out.println("Tax: " + (100 + (double)(value-5000)*0.08));
+        }else if(value<=55000){
+            System.out.println("Tax: " + (1700 + (double)(value-25000)*0.1));
+        }else if(value<=200000){
+            System.out.println("Tax: " + (4700 + (double)(value-55000)*0.12));
+        }else if(value<=1000000){
+            System.out.println("Tax: " + (22100 + (double)(value-200000)*0.15));
+        }else if(value>1000000){
+            System.out.println("Tax: " + (142100 + (double)(value-1000000)*0.17));
+        }
 
     }
 }
