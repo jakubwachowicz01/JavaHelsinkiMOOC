720442fd9dd3023c813403cfc6316545ae260f51
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-11-17 12:01:22 +0100

    completed Practising string "equals"

--- part01-Part01_33.Password/src/main/java/Password.java
+++ part01-Part01_33.Password/src/main/java/Password.java
@@ -6,6 +6,13 @@ public class Password {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Write your program here 
+        System.out.println("Password?");
+        String pass = String.valueOf(scan.nextLine());
+
+        if (pass.equals("Caput Draconis")){
+            System.out.println("Welcome!");
+        }else{
+            System.out.println("Off with you!");
+        }
     }
 }
