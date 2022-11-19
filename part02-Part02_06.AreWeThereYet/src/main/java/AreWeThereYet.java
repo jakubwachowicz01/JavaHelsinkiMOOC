c7692ea13f23a575b84193ab9e4a55435e3b187b
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-11-19 12:02:46 +0100

    completed

--- part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
+++ part02-Part02_06.AreWeThereYet/src/main/java/AreWeThereYet.java
@@ -5,6 +5,12 @@ public class AreWeThereYet {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        while(true){
+            System.out.println("Give a number:");
+            int input = Integer.valueOf(scanner.nextLine());
+            if(input==4){
+                break;
+            }
+        }
     }
 }
--- part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
+++ part02-Part02_07.OnlyPositives/src/main/java/OnlyPositives.java
@@ -5,6 +5,17 @@ public class OnlyPositives {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        while(true){
+            System.out.println("Give a number:");
+            int input = Integer.valueOf(scanner.nextLine());
+            if(input<0){
+                System.out.println("Unsuitable number");
+                continue;
+            } else if (input>0) {
+                System.out.println(Math.pow(input,2));
+            }else{
+                break;
+            }
+        }
     }
 }
