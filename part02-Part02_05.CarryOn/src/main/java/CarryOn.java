708197d2d85dc627336f970daf7b42f599b0c320
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-11-19 11:20:33 +0100

    completed

--- part02-Part02_05.CarryOn/src/main/java/CarryOn.java
+++ part02-Part02_05.CarryOn/src/main/java/CarryOn.java
@@ -5,6 +5,12 @@ public class CarryOn {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        while(true){
+            System.out.println("Shall we carry on?");
+            String input = scanner.nextLine();
+            if(input.equals("no")){
+                break;
+            }
+        }
     }
 }
