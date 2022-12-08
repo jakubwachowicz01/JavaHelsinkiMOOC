775c01183edecbf8b7f45749a30e686cbf0996c0
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-12-08 22:55:37 +0100

    completed

--- part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -5,6 +5,16 @@ public class NumberOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int count = 0;
+        while(true){
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            if(number==0){
+                break;
+            }else {
+                count++;
+            }
+        }
+        System.out.println("Number of numbers: " + count);
     }
 }
