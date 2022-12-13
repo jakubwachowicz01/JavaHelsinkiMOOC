e154a4035580265b0ae9db507d6bd27d455ca78a
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-12-13 13:02:42 +0100

    completed

--- part02-Part02_25.Division/src/main/java/Division.java
+++ part02-Part02_25.Division/src/main/java/Division.java
@@ -7,9 +7,10 @@ public class Division {
         // Once you have implemented the division method, you can
         // try it out here. Fopr example division(3,5);
         // should print "0.6"
-
-        // division(3, 5);
+        division(3, 5);
     }
 
-    // implement the method here
+    public static void division(int numerator, int denominator){
+        System.out.println(1.0*numerator/denominator);
+    }
 }
