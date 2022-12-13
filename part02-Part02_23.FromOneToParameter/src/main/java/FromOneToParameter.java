16a7de4ce25bf7181b572bdfd551931e57560997
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-12-13 12:54:28 +0100

    completed

--- part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
+++ part02-Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
@@ -3,7 +3,12 @@
 public class FromOneToParameter {
 
     public static void main(String[] args) {
-
+        printUntilNumber(5);
     }
 
+    public static void printUntilNumber(int number){
+        for(int i =1 ; i<=number; i++){
+            System.out.println(i);
+        }
+    }
 }
