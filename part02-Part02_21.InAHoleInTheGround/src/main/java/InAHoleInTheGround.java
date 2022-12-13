e15e9b1faf4b595e1a9d32a397d5c5e43c0883cd
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-12-13 12:51:37 +0100

    completed

--- part02-Part02_21.InAHoleInTheGround/src/main/java/InAHoleInTheGround.java
+++ part02-Part02_21.InAHoleInTheGround/src/main/java/InAHoleInTheGround.java
@@ -3,8 +3,8 @@ public class InAHoleInTheGround {
     public static void main(String[] args) {
         printText();
     }
-    
+
     public static void printText() {
-        // Write some code in here
+        System.out.println("In a hole in the ground there lived a method");
     }
 }
--- part02-Part02_22.Reprint/src/main/java/Reprint.java
+++ part02-Part02_22.Reprint/src/main/java/Reprint.java
@@ -8,11 +8,16 @@ public class Reprint {
         // then call the printText-method multiple times with a while-loop
         
         Scanner scanner = new Scanner(System.in);
+        System.out.println("How many times?");
+        int number = Integer.valueOf(scanner.nextLine());
+        for(int i = 0 ;i<number;i++){
+            printText();
+        }
     }
     
     // Don't change the next line that defines the method!
     // (We aren't giving a method a parameter yet)
     public static void printText() {
-        // write some code here
+        System.out.println("In a hole in the ground there lived a method");
     }
 }
