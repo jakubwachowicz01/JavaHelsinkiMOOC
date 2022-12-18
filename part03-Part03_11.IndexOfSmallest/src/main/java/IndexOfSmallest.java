f7e3ea5c9c970d6ce932f365743a2932787f1414
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-12-18 13:34:00 +0100

    completed

--- part03-Part03_11.IndexOfSmallest/src/main/java/IndexOfSmallest.java
+++ part03-Part03_11.IndexOfSmallest/src/main/java/IndexOfSmallest.java
@@ -6,8 +6,28 @@ public class IndexOfSmallest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
 
-        // implement here a program that reads user input
+        while(true){
+            int input = Integer.valueOf(scanner.nextLine());
+            if(input == 9999){
+                break;
+            }
+            numbers.add(input);
+        }
+        int smallest = numbers.get(0);
+        for(int i =1; i< numbers.size();i++){
+            if(numbers.get(i)<smallest){
+                smallest=numbers.get(i);
+            }
+        }
+        System.out.println("Smallest number: "+smallest);
+        for(int i =0; i< numbers.size();i++) {
+            if (numbers.get(i) == smallest) {
+                System.out.println("Found at index: " + i);
+            }
+        }
+                // implement here a program that reads user input
         // until the user enters 9999
         
         // after that, the program prints the smallest number
