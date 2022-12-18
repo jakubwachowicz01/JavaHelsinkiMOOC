67689c8ea5bd420115f3a5c04ec1a915469f2994
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-12-18 13:22:50 +0100

    completed

--- part03-Part03_10.IndexOf/src/main/java/IndexOf.java
+++ part03-Part03_10.IndexOf/src/main/java/IndexOf.java
@@ -17,8 +17,13 @@ public class IndexOf {
             list.add(input);
         }
 
-        System.out.println("");
+        System.out.println("Search for? ");
+        int search = Integer.valueOf(scanner.nextLine());
 
-        // implement here finding the indices of a number
+        for(int i =0; i<list.size(); i++){
+            if(list.get(i)==search){
+                System.out.println(search + " is at index " + i);
+            }
+        }
     }
 }
