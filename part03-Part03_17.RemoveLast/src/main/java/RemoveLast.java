8f6eaf5e4b35786256fbf3dc64978eca3bc6e526
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-12-19 12:08:19 +0100

    completed

--- part03-Part03_17.RemoveLast/src/main/java/RemoveLast.java
+++ part03-Part03_17.RemoveLast/src/main/java/RemoveLast.java
@@ -6,5 +6,10 @@ public class RemoveLast {
     public static void main(String[] args) {
         // Try your method in here
     }
-
+    public static void removeLast(ArrayList<String> strings){
+        if(strings.size()==0){
+            return;
+        }
+        strings.remove(strings.size()-1);
+    }
 }
