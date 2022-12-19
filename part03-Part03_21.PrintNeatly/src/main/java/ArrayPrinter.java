df407301e7079a14fff06612232c82e6a3b19e46
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-12-19 13:10:26 +0100

    completed

--- part03-Part03_21.PrintNeatly/src/main/java/ArrayPrinter.java
+++ part03-Part03_21.PrintNeatly/src/main/java/ArrayPrinter.java
@@ -8,6 +8,10 @@ public class ArrayPrinter {
     }
 
     public static void printNeatly(int[] array) {
-        // Write some code in here
+        for (int i =0;i<array.length-1;i++){
+
+            System.out.print(array[i] + ", ");
+        }
+        System.out.print(array[array.length-1]);
     }
 }
--- part03-Part03_22.PrintInStars/src/main/java/Printer.java
+++ part03-Part03_22.PrintInStars/src/main/java/Printer.java
@@ -8,7 +8,12 @@ public class Printer {
     }
 
     public static void printArrayInStars(int[] array) {
-        // Write some code in here
+        for(int i =0; i< array.length;i++){
+            for (int j =0; j<=array[i]-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
 }
