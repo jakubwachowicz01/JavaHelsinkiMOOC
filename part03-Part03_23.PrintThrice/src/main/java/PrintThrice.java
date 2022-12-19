34af7f7ad82927db6e1df2d7e674701d36a17077
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-12-19 13:17:45 +0100

    completed

--- part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
+++ part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
@@ -6,7 +6,9 @@ public class PrintThrice {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Write your program here
+        String input = scanner.nextLine();
+        for(int i =0;i<3;i++)
+        System.out.print(input);
 
     }
 }
