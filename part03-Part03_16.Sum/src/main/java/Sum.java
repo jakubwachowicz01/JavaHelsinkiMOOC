c32a9cd3008e4c67ac9c72482c6012fc7b85900e
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-12-19 11:53:32 +0100

    completed

--- part03-Part03_16.Sum/src/main/java/Sum.java
+++ part03-Part03_16.Sum/src/main/java/Sum.java
@@ -6,5 +6,11 @@ public class Sum {
     public static void main(String[] args) {
         // Try your method here
     }
-
+    public static int sum(ArrayList<Integer> numbers){
+        int sum =0;
+        for(int number:numbers){
+            sum = sum+ number;
+        }
+        return sum;
+    }
 }
