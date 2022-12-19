fab5782810ec0a906bce3a09ad3eb3f2cd3dca34
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-12-19 13:40:42 +0100

    completed

--- part03-Part03_24.IsItTrue/src/main/java/IsItTrue.java
+++ part03-Part03_24.IsItTrue/src/main/java/IsItTrue.java
@@ -5,6 +5,11 @@ public class IsItTrue {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String input = scanner.nextLine();
+        if(input.equals("true")){
+            System.out.println("You got it right!");
+        }else {
+            System.out.println("Try again!");
+        }
     }
 }
