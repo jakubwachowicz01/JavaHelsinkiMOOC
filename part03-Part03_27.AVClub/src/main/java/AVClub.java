ff655f5bc621b5132e646f1c689715b760b08d05
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-12-20 17:10:43 +0100

    completed

--- part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -5,7 +5,18 @@ public class AVClub {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        while(true){
+            String input= scanner.nextLine();
+            if(input.equals("")){
+                break;
+            }
+            String[] words=input.split(" ");
+            for(String word:words){
+                if(word.contains("av")){
+                    System.out.println(word);
+                }
+            }
+        }
 
     }
 }
