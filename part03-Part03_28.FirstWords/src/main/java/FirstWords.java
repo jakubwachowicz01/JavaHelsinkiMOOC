ea70f388dad62214ffacab0e7ac55b5c4c5107ed
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-12-20 17:15:01 +0100

    completed

--- part03-Part03_28.FirstWords/src/main/java/FirstWords.java
+++ part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -5,7 +5,14 @@ public class FirstWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        while(true){
+            String input = scanner.nextLine();
+            if(input.equals("")){
+                break;
+            }
+            String[] words = input.split(" ");
+            System.out.println(words[0]);
+        }
 
     }
 }
