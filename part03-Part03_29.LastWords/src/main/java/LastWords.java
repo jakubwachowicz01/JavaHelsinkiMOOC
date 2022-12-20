d875f7cde764c2d41fa9ebb85f53f1eec2f4488b
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-12-20 17:18:53 +0100

    completed

--- part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -5,7 +5,12 @@ public class LastWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        while(true){
+            String input = scanner.nextLine();
+            if(input.equals("")) break;
+            String[] words = input.split(" ");
+            System.out.println(words[words.length-1]);
+        }
 
     }
 }
