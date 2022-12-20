3149316bc026053f100021001fd0665feba284ee
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2022-12-20 17:06:35 +0100

    completed

--- part03-Part03_25.Login/src/main/java/Login.java
+++ part03-Part03_25.Login/src/main/java/Login.java
@@ -5,6 +5,14 @@ public class Login {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Enter username: ");
+        String username = scanner.nextLine();
+        System.out.println("Enter password: ");
+        String password = scanner.nextLine();
+        if((username.equals("alex") && password.equals("sunshine")) || (username.equals("emma") && password.equals("haskell"))){
+            System.out.println("You have successfully logged in!");
+        }else{
+            System.out.println("Incorrect username or password!");
+        }
     }
 }
--- part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LineByLine {
@@ -6,6 +7,16 @@ public class LineByLine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while(true){
+            String input = scanner.nextLine();
+            if(input.equals("")){
+                break;
+            }
+            String[] words=input.split(" ");
+            for(String word:words){
+                System.out.println(word);
+            }
+        }
 
     }
 }
