e74337e84bf668ba5cdcd4cdbb0d003ce4dc55e1
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-01-06 17:22:38 +0100

    completed

--- part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -2,7 +2,8 @@
 public class YourFirstAccount {
 
     public static void main(String[] args) {
-        // Do not touch the code in Account.java
-        // Write your program here
+        Account account = new Account("Jakub's account",100);
+        account.deposit(20);
+        System.out.println(account);
     }
 }
