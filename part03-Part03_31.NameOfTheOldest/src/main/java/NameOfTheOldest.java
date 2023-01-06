2ba209fe18afc8cd2b1603b0146aa96827d41a90
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-01-06 16:04:01 +0100

    completed

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -5,7 +5,19 @@ public class NameOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        int oldest = 0;
+        String name="x";
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) break;
+            String[] parts = input.split(",");
+            if (Integer.valueOf(parts[1]) > oldest) {
+                oldest = Integer.valueOf(parts[1]);
+                name = parts[0];
+            }
+        }
+        System.out.println("Name of the oldest: ");
+        System.out.print(name);
     }
 }
+
--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -6,7 +6,12 @@ public class PersonalDetails {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int sum=0;
+        int max = 0;
+        while(true){
+            String input = scanner.nextLine();
 
+        }
 
     }
 }
