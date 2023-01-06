dae650ef8a358191557e303ad59db558084de388
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-01-06 17:38:37 +0100

    completed

--- part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
+++ part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -2,7 +2,11 @@
 public class YourFirstBankTransfer {
 
     public static void main(String[] args) {
-        // Do not touch the code in Account.java
-        // write your program here
+        Account matthew = new Account("Matthews account",1000);
+        Account myAccount = new Account("My account",0.0);
+        matthew.withdrawal(100.0);
+        myAccount.deposit(100.0);
+        System.out.println(matthew);
+        System.out.println(myAccount);
     }
 }
