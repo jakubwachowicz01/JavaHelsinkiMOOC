38cce7eab2662dc246ce88e0f291afceeb5ce19e
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-01-06 16:17:45 +0100

    completed

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -7,11 +7,20 @@ public class PersonalDetails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int sum=0;
-        int max = 0;
+        int ammount=0;
+        String name ="";
         while(true){
             String input = scanner.nextLine();
-
+            if(input.equals("")) break;
+            String[] pieces = input.split(",");
+            sum += Integer.valueOf(pieces[1]);
+            if(pieces[0].length()>name.length()){
+                name = pieces[0];
+            }
+            ammount++;
         }
+        System.out.println("Longest name: " + name);
+        System.out.println("Average of the birth years: " + 1.0*sum/ammount);
 
     }
 }
