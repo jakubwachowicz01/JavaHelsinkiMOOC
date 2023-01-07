a3c35550ee072bfaf4af0fb601a6a3fe3ffc1405
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-01-07 14:46:59 +0100

    completed

--- part04-Part04_08.DecreasingCounter/src/main/java/DecreasingCounter.java
+++ part04-Part04_08.DecreasingCounter/src/main/java/DecreasingCounter.java
@@ -13,9 +13,13 @@ public class DecreasingCounter {
     }
 
     public void decrement() {
-        // write the method implementation here
-        // the aim is to decrement the value of the counter by one
+        if(this.value>0){
+            this.value--;
+        }
+
     }
 
-    // the other methods go here
+    public void reset(){
+        this.value=0;
+    }
 }
