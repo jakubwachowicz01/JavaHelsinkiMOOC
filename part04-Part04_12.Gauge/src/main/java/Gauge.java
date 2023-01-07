f3f08c3246330309dfe00e3fbe3407cb5dd02da6
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-01-07 15:33:32 +0100

    completed

---
+++ part04-Part04_12.Gauge/src/main/java/Gauge.java
@@ -0,0 +1,28 @@
+public class Gauge {
+    private int value;
+
+    public Gauge() {
+
+    }
+
+    public void increase(){
+        if(this.value<5){
+            this.value++;
+        }
+    }
+    public void decrease(){
+        if(this.value>0){
+            this.value--;
+        }
+    }
+    public int value(){
+        return this.value;
+    }
+    public boolean full(){
+        if(this.value==5){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
