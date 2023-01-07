0f2cd1b5d4861cd9c7756f2e57f656a2a924a1c4
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-01-07 18:55:07 +0100

    completed

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -5,6 +5,25 @@ public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Statistics all = new Statistics();
+        Statistics even = new Statistics();
+        Statistics odd = new Statistics();
+        System.out.println("Enter numbers: ");
+        while(true){
+            int input = Integer.valueOf(scanner.nextLine());
+            if(input==-1){
+                break;
+            }else if(input%2==0){
+                even.addNumber(input);
+            }else{
+                odd.addNumber(input);
+            }
+            all.addNumber(input);
+        }
+        System.out.println("Sum: " + all.sum());
+        System.out.println("Sum of even numbers: " + even.sum());
+        System.out.println("Sum of odd numbers: " + odd.sum());
+
         // you can write test code here
         // however, remove all unnecessary code when doing the final parts of the exercise
 
--- part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,3 +1,35 @@
 
 public class Statistics {
+    private int count;
+    private int sum;
+    public Statistics() {
+        this.count = count;
+        this.sum = 0;
+    }
+
+    public void addNumber(int number){
+        count++;
+        this.sum += number;
+    }
+
+    public int getCount(){
+        return this.count;
+    }
+
+    public int sum(){
+        if(count!=0){
+            return this.sum;
+        }else{
+            return 0;
+        }
+
+    }
+    public double average(){
+        if(count!=0){
+            return 1.0*this.sum/this.count;
+        }else{
+            return 0;
+        }
+
+    }
 }
