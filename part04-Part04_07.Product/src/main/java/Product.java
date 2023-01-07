3518a26e5a9274eec3d9a84f12ef30eb7c5c23cf
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-01-07 14:42:01 +0100

    completed

---
+++ part04-Part04_07.Product/src/main/java/Product.java
@@ -0,0 +1,14 @@
+public class Product {
+    private double price;
+    private int quantity;
+    private String name;
+    public Product(String initialName, double initialPrice, int initialQuantity){
+        this.price = initialPrice;
+        this.quantity = initialQuantity;
+        this.name = initialName;
+    }
+
+    public void printProduct(){
+        System.out.println(this.name + ", " + this.price+ ", " + this.quantity +" pcs");
+    }
+}
