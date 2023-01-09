5d58c8e34a46f576428557d4f9a1a577f0c31e33
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-01-09 13:48:27 +0100

    completed

--- part04-Part04_16.PaymentCard/src/main/java/MainProgram.java
+++ part04-Part04_16.PaymentCard/src/main/java/MainProgram.java
@@ -2,6 +2,22 @@
 public class MainProgram {
 
     public static void main(String[] args) {
+        PaymentCard paulsCard = new PaymentCard(20);
+        PaymentCard mattsCard = new PaymentCard(30);
+
+        paulsCard.eatHeartily();
+        mattsCard.eatAffordably();
+        System.out.println("Paul: "+paulsCard);
+        System.out.println("Matt: "+mattsCard);
+        paulsCard.addMoney(20);
+        mattsCard.eatHeartily();
+        System.out.println("Paul: "+paulsCard);
+        System.out.println("Matt: "+mattsCard);
+        paulsCard.eatAffordably();
+        paulsCard.eatAffordably();
+        mattsCard.addMoney(50);
+        System.out.println("Paul: "+paulsCard);
+        System.out.println("Matt: "+mattsCard);
         // Scanner scanner = new Scanner(System.in);
         
         // Here you can write code to test that PaymentCard works as intended
