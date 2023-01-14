b651a15ed2a468b9f99a85c62f6a0e953e7e234c
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-01-14 19:50:45 +0100

    completed

--- part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -8,6 +8,20 @@ public class Main {
 
         ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-
+        while(true){
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+            if(name.equals("")) break;
+            System.out.print("Duration: ");
+            int duration = Integer.valueOf(scanner.nextLine());
+            programs.add(new TelevisionProgram(name, duration));
+        }
+        System.out.print("Program's maximum duration? ");
+        int maxDuration = Integer.valueOf(scanner.nextLine());
+        for(TelevisionProgram program: programs){
+            if(program.getDuration() <= maxDuration){
+                System.out.println(program);
+            }
+        }
     }
 }
