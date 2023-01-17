9f5f06ef1cf30f15ceedcafa39877e5db446a4ee
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-01-17 16:35:50 +0100

    completed

--- part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -5,6 +5,11 @@ public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        while(true){
+            String input = scanner.nextLine();
+            if(input.equals("end")) break;
+            int cube = Integer.valueOf(input)*Integer.valueOf(input)*Integer.valueOf(input);
+            System.out.println(cube);
+        }
     }
 }
