c6318bfaddd46a3227ee8c709359a6cebefd37b2
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-01-30 14:15:51 +0100

    completed

--- part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
+++ part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
@@ -1,10 +1,18 @@
 
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class PrintingAFile {
 
     public static void main(String[] args) {
-
+        try (Scanner scanner = new Scanner(Paths.get("data.txt"))){
+            while(scanner.hasNextLine()) {
+                String row = scanner.nextLine();
+                System.out.println(row);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 }
