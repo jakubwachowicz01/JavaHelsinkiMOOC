78d244da3ce8ea21671b6107b6d5169dbac33167
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-01-30 14:49:26 +0100

    completed

--- part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -6,6 +6,19 @@ public class PrintingASpecifiedFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Which file should have its contents printed?");
+        String input = scanner.nextLine();
+        try (Scanner scannerFiles = new Scanner(Paths.get(input))) {
 
+            // we read the file until all lines have been read
+            while (scannerFiles.hasNextLine()) {
+                // we read one line
+                String row = scannerFiles.nextLine();
+                // we print the line that we read
+                System.out.println(row);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
