2f87bf398045ac786c50a19cb44b4ffed04af17e
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-01-30 16:39:00 +0100

    completed

--- part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -1,4 +1,5 @@
 
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
@@ -10,8 +11,28 @@ public class IsItInTheFile {
         System.out.println("Name of the file:");
         String file = scanner.nextLine();
 
+
+
         System.out.println("Search for:");
         String searchedFor = scanner.nextLine();
+        boolean found = false;
+        try(Scanner scannerFiles = new Scanner(Paths.get(file))){
 
+            while(scannerFiles.hasNextLine()){
+                String row = scannerFiles.nextLine();
+                if(searchedFor.equals(row)){
+                    found = true;
+                }
+            }
+
+
+            if(found){
+                System.out.println("Found!");
+            } else {
+                System.out.println("Not found.");
+            }
+        }catch (Exception e){
+            System.out.println("Reading the file " + e.getMessage() + " failed");
+        }
     }
 }
