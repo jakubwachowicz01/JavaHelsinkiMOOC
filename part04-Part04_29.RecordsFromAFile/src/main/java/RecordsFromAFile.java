7cbf9b99dc0fcf8bfb15b01e1d3e10d039cebef0
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-02-01 15:16:49 +0100

    completed

--- part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -1,4 +1,6 @@
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
@@ -6,6 +8,21 @@ public class RecordsFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Name of the file?");
+        String input = scanner.nextLine();
+        try(Scanner fileScanner = new Scanner(Paths.get(input))){
+            while(fileScanner.hasNextLine()){
+                String line = fileScanner.nextLine();
+                String[] parts = line.split(",");
+                System.out.print(parts[0] + ", age: " + parts[1]);
+                if(Integer.valueOf(parts[1])==1){
+                    System.out.println(" year");
+                }else if(Integer.valueOf(parts[1])>1 || Integer.valueOf(parts[1])==0){
+                    System.out.println(" years");
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
