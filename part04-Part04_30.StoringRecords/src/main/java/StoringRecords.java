40f4791a61a7c1babd54e3fe1a711c6920cb8459
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-02-03 20:40:54 +0100

    completed

--- part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
+++ part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
@@ -1,4 +1,5 @@
 
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -23,6 +24,21 @@ public class StoringRecords {
     public static ArrayList<Person> readRecordsFromFile(String file) {
         ArrayList<Person> persons = new ArrayList<>();
 
+        try(Scanner fileScanner = new Scanner(Paths.get(file))){
+            while(fileScanner.hasNextLine()){
+                String line = fileScanner.nextLine();
+                String pieces[] = line.split(",");
+                String name = pieces[0];
+                int age = Integer.valueOf(pieces[1]);
+                persons.add(new Person(name,age));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for (Person person: persons
+             ) {
+            System.out.println(person);
+        }
         // Write here the code for reading from file
         // and printing the read records
         return persons;
