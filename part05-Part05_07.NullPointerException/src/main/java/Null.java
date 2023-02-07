8b1b9c34d5ddbba7fc9939aaffdbed6d6e7d83bb
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-02-07 13:03:32 +0100

    completed

---
+++ part05-Part05_07.NullPointerException/src/main/java/Null.java
@@ -0,0 +1,5 @@
+public class Null {
+public String doThing(){
+    return "a";
+}
+}
--- part05-Part05_07.NullPointerException/src/main/java/NullPointerExceptionProgram.java
+++ part05-Part05_07.NullPointerException/src/main/java/NullPointerExceptionProgram.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 public class NullPointerExceptionProgram {
 
     public static void main(String[] args) {
+        Null nulle = null;
+        System.out.println(nulle.doThing());
         // Implement a program, or modify a program so, that it causes 
         // the NullPointerException -error
 
