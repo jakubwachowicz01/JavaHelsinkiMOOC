7660937d20e3f0f1d27e01956437e8bab7c859ad
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-02-07 15:01:29 +0100

    completed

--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -11,4 +11,29 @@ public class Apartment {
         this.princePerSquare = pricePerSquare;
     }
 
+    public boolean largerThan(Apartment compared){
+        if(this.squares> compared.squares){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public int priceDifference(Apartment compared){
+        int priceDif = (this.squares*this.princePerSquare)-(compared.squares*compared.princePerSquare);
+        if(priceDif<0){
+            return priceDif*(-1);
+        }else{
+            return priceDif;
+        }
+    }
+
+    public boolean moreExpensiveThan(Apartment compared){
+        if(this.squares*this.princePerSquare>compared.squares*compared.princePerSquare){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
