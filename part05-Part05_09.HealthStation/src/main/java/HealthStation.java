650d3da1e312a8fa946c2dd9880506bd0b3fc80c
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-02-07 13:26:45 +0100

    completed

--- part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,21 @@
 
 public class HealthStation {
 
+    private int counter;
 
-    public int weigh(Person person) {
-        // return the weight of the person passed as the parameter
-        return -1;
+    public HealthStation() {
+        this.counter = 0;
     }
 
+    public int weigh(Person person) {
+        this.counter++;
+        return person.getWeight();
+    }
+    public void feed(Person person){
+        person.setWeight(person.getWeight()+1);
+    }
+
+    public int weighings(){
+        return this.counter;
+    }
 }
--- part05-Part05_09.HealthStation/src/main/java/Main.java
+++ part05-Part05_09.HealthStation/src/main/java/Main.java
@@ -2,6 +2,21 @@
 public class Main {
 
     public static void main(String[] args) {
-        // write experimental code here to check how your program functions
+        HealthStation childrensHospital = new HealthStation();
+
+        Person ethan = new Person("Ethan", 1, 110, 7);
+        Person peter = new Person("Peter", 33, 176, 85);
+
+        System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
+        System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
+
+        childrensHospital.feed(ethan);
+        childrensHospital.feed(ethan);
+        childrensHospital.feed(ethan);
+
+        System.out.println("");
+
+        System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
+        System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
     }
 }
