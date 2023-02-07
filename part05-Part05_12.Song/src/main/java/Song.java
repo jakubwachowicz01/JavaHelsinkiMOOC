a83b81745e45e0332ce4418d72c6cddceeb25de5
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-02-07 15:34:22 +0100

    completed

--- part05-Part05_12.Song/src/main/java/Song.java
+++ part05-Part05_12.Song/src/main/java/Song.java
@@ -16,5 +16,21 @@ public class Song {
         return this.artist + ": " + this.name + " (" + this.durationInSeconds + " s)";
     }
 
+    public boolean equals(Object compared){
+        if(this==compared) return true;
+        if(!(compared instanceof Song)){
 
+            return false;
+        }
+        Song anotherSong = (Song) compared;
+        if(
+                this.artist.equals(anotherSong.artist) &&
+                        this.name.equals(anotherSong.name) &&
+                        this.durationInSeconds == anotherSong.durationInSeconds
+        ){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
