effab610da733524fcdc03578a9ddc5779f09af4
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-02-07 14:26:53 +0100

    completed

--- part05-Part05_08.BiggestPetShop/src/main/java/Person.java
+++ part05-Part05_08.BiggestPetShop/src/main/java/Person.java
@@ -19,7 +19,7 @@ public class Person {
 
     @Override
     public String toString() {
-        return this.name;
+        return this.name + ", has a friend called " + pet.getName() + " (" + pet.getBreed()+")";
     }
 
 }
