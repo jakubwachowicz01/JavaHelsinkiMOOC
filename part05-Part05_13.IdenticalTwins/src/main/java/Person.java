24aa610578d106dfb7b3b64d964093252279d1e3
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-02-08 11:05:20 +0100

    completed

--- part05-Part05_13.IdenticalTwins/src/main/java/Person.java
+++ part05-Part05_13.IdenticalTwins/src/main/java/Person.java
@@ -15,5 +15,21 @@ public class Person {
         this.weight = weight;
     }
 
+    public boolean equals(Object compared){
+        if(this == compared) return true;
+
+        Person personCompared = (Person) compared;
+        if(this.name.equals(personCompared.name)&&
+                this.height== personCompared.height&&
+                this.weight== personCompared.weight&&
+                this.birthday.getDay()==personCompared.birthday.getDay()&&
+                this.birthday.getYear()==personCompared.birthday.getYear()&&
+                this.birthday.getMonth()==personCompared.birthday.getMonth()
+        ){
+            return true;
+        }else{
+            return false;
+        }
+    }
     // implement an equals method here for checking the equality of objects
 }
