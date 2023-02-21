7b9b1dbd203424ecc6d0b6990ad45e0f8fb4ee7c
Jakub Wachowicz <jakubwachowicz01@gmail.com>
2023-02-21 15:27:26 +0100

    completed

--- part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
+++ part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
@@ -1,6 +1,43 @@
+import org.junit.Before;
+import org.junit.Test;
 
+import static org.junit.Assert.*;
 
 
 public class ExerciseManagementTest {
+    private ExerciseManagement management;
+    @Before
+    public void initialize(){
+        management = new ExerciseManagement();
+    }
+    @Test
+    public void exerciseListEmptyAtBeginning() {
+        assertEquals(0, management.exerciseList().size());
+    }
 
+    @Test
+    public void addingExerciseGrowsListByOne(){
+        management.add("Write as test");
+        assertEquals(1, management.exerciseList().size());
+    }
+
+    @Test
+    public void addedExerciseIsInList(){
+        management.add("Write a test");
+        assertTrue(management.exerciseList().contains("Write a test"));
+    }
+
+    @Test
+    public void exerciseCanBeMarkedAsCompleted(){
+        management.add("New exercise");
+        management.markAsCompleted("New exercise");
+        assertTrue(management.isCompleted("New exercise"));
+    }
+
+    @Test
+    public void ifNotMarkedCompletedIsNotCompleted() {
+        management.add("New exercise");
+        management.markAsCompleted("New exercise");
+        assertFalse(management.isCompleted("Some exercise"));
+    }
 }
